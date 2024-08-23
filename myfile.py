09d4662077999b95dec7c7f3cec62debc204ca56
Poorna Subramanian <poorna.s.subramanian@gmail.com>
2024-08-23 14:55:00 +0000

    Step 5- Added an error.

--- myfile.py
+++ myfile.py
@@ -1,6 +1,9 @@
 #printing array items
 def error1():
-    array = [1, 6, 3, 7, 9];
-    print(array[0])
+    array = [1, 6, 3, 7, 9]
+    sum=0
+    for i in range (0,6):
+        sum+=array[i]
+    return sum
 
 error1()
