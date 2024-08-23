891058ce9ccfcfcf9e4b0e63aab85e3550ca0a21
Poorna Subramanian <poorna.s.subramanian@gmail.com>
2024-08-23 15:34:02 +0000

    Corrected the error in code.

--- myfile.py
+++ myfile.py
@@ -2,8 +2,8 @@
 def error1():
     array = [1, 6, 3, 7, 9]
     sum=0
-    for i in range (0,6):
+    for i in range (0,5):
         sum+=array[i]
     return sum
 
-error1()
+print("Sum : ",error1())
