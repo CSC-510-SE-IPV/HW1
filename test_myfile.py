900fa7c4110eef3a05ed015aad2ebe7e18da6a18
Ishani Rajput <167826767+ishani-rajput@users.noreply.github.com>
2024-08-24 15:46:42 +0000

    Failing test case commented out

--- test_myfile.py
+++ test_myfile.py
@@ -3,5 +3,5 @@ import myfile
 def test_sum():
     assert myfile.array_sum([1,2,3]) == 6
 
-def test_sum_fail():
-    assert myfile.array_sum([1,2,3]) == 7
+# def test_sum_fail():
+#     assert myfile.array_sum([1,2,3]) == 7
