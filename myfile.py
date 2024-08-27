d036c212b4296d9d64626d77aa66562ae249820c
Poorna Subramanian <poorna.s.subramanian@gmail.com>
2024-08-27 22:02:21 +0000

    Corrected error

--- myfile.py
+++ myfile.py
@@ -1,6 +1,7 @@
 #Printing the sum of the given array
 def array_sum(array):
-    if (len(array)==0) return 0;
+    if (len(array)==0):
+        return 0
     sum=0
     for i in range (0,len(array)):
         sum+=array[i]
