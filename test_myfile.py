57807ce19647fa66f384cbd2d3e352304119bbfc
Ishani Rajput <167826767+ishani-rajput@users.noreply.github.com>
2024-08-27 19:05:24 +0000

    Junit removed from workflows

--- test_myfile.py
+++ test_myfile.py
@@ -1,7 +1,7 @@
 import myfile
 
-def test_sum():
-    assert myfile.array_sum([1,2,3]) == 6
+# def test_sum():
+#     assert myfile.array_sum([1,2,3]) == 6
 
 def test_sum_fail():
     assert myfile.array_sum([1,2,3]) == 7
