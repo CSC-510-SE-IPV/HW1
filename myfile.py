4ef8f4c0a2e8a3ed0d2bed00f0884ba2bc96b45d
Poorna Subramanian <poorna.s.subramanian@gmail.com>
2024-08-27 22:06:03 +0000

    Verifying coverage.

--- myfile.py
+++ myfile.py
@@ -1,10 +1,10 @@
 #Printing the sum of the given array
 def array_sum(array):
-    if (len(array)==0):
-        return 0
     sum=0
     for i in range (0,len(array)):
         sum+=array[i]
+    if sum > 10:
+        print("Sum is greater than 10")
     return sum
 
 print("Sum : ",array_sum([1,2,3]))
